9d5bfa48a20d8c5361a0b1f5d4e3e2318839f3fd
Josh Driver <keeperofdakeys@gmail.com>
2017-09-19 21:53:21 +0930

    Convert to derive api

--- src/lib.rs
+++ src/lib.rs
@@ -1,81 +1,59 @@
 #![allow(non_snake_case)]
 #![allow(non_camel_case_types)]
 
-#[macro_use]
 extern crate asn1_cereal;
+#[macro_use]
+extern crate asn1_cereal_derive;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
 
 use asn1_cereal::OctetString;
 
+#[derive(Asn1Info, BerSerialize, BerDeserialize)]
+#[asn1(log)]
 struct LDAPMessage {
   messageID: i32,
   protocolOp: ProtocolOp,
-  controls: Option<Controls>,
+  // controls: Option<Controls>,
 }
 
-ber_sequence!(
-  LDAPMessage,
-  "LDAPMessage",
-  messageID;
-  protocolOp;
-  controls ([0] OPTIONAL);
-);
-
+#[derive(Asn1Info, BerSerialize, BerDeserialize)]
+#[asn1(log)]
 enum ProtocolOp {
   bindRequest(BindRequest),
 }
 
-ber_choice!(
-  ProtocolOp,
-  "CHOICE",
-  bindRequest
-);
-
+#[derive(Asn1Info, BerSerialize, BerDeserialize)]
+#[asn1(log)]
 struct LdapDN(OctetString);
-ber_alias!(LdapDN ::= OctetString, "LdapDN");
 
+#[derive(Asn1Info, BerSerialize, BerDeserialize)]
+#[asn1(log)]
 struct LdapOID(OctetString);
-ber_alias!(LdapOID ::= OctetString, "LdapOID");
 
+#[derive(Asn1Info, BerSerialize, BerDeserialize)]
+#[asn1(log)]
 struct Controls(Vec<Control>);
 
-ber_alias!(Controls ::= Vec<Control>, "Controls");
-
+#[derive(Asn1Info, BerSerialize, BerDeserialize)]
+#[asn1(log)]
 struct Control {
   controlType: LdapOID,
   criticality: bool,
-  controlValue: Option<OctetString>,
+  // controlValue: Option<OctetString>,
 }
 
-ber_sequence!(
-  Control,
-  "Control",
-  controlType;
-  criticality (DEFAULT false);
-  controlValue ([0] OPTIONAL);
-);
-
+#[derive(Asn1Info, BerSerialize, BerDeserialize)]
+#[asn1(tag="[APPLICATION 8]", log)]
 struct BindRequest {
   version: u32,
   name: LdapDN,
   authentication: AuthenticationChoice,
 }
 
-ber_sequence!(
-  BindRequest,
-  [APPLICATION 0],
-  "BindRequest",
-  version;
-  name;
-  authentication;
-);
-
+#[derive(Asn1Info, BerSerialize, BerDeserialize)]
+#[asn1(log)]
 enum AuthenticationChoice {
   simple(OctetString),
 }
-
-ber_choice!(
-  AuthenticationChoice,
-  "AuthenticationChoice",
-  simple
-);
-
